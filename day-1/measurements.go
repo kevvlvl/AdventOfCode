71fd3d96c15cd50d38b731a43242e8e7d5ed7490
Kevork <ksayad@gmail.com>
2021-12-22 23:00:19 -0500

    day 1 puzzle

---
+++ day-1/measurements.go
@@ -0,0 +1,40 @@
+package day_1
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+func Day1_measurements() {
+
+	fmt.Println("day1_measurements() Start")
+
+	var content, err = ioutil.ReadFile("day-1/puzzle-input.txt")
+
+	if err != nil {
+		fmt.Println("  FATAL: - Failed to read file")
+	}
+
+	var stringData = bytes.Split(content, []byte{'\n'})
+
+	fmt.Println("File contents length: ", len(stringData))
+
+	inc := 0
+	dec := 0
+
+	for i := 1; i < len(stringData); i++ {
+
+		intCurrent, _ := strconv.Atoi(string(stringData[i]))
+		intPrev, _ := strconv.Atoi(string(stringData[i-1]))
+
+		if intCurrent > intPrev {
+			inc++
+		} else {
+			dec++
+		}
+	}
+
+	fmt.Println("day1_measurements() End - Increases = ", inc, ", Decreases = ", dec)
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "AdventOfCode/day-1"
+
+func main() {
+
+	day_1.Day1_measurements()
+
+}
