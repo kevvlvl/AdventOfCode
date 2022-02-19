629824ee7005db4df58a6bf1ed19504e838762ed
Kevork <ksayad@gmail.com>
2022-02-18 23:05:49 -0500

    day 4 part 1

--- day-4/bingo.go
+++ day-4/bingo.go
@@ -4,16 +4,19 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
-var numbers []string
+var numbers []int
+var lastNumberCalled int
 var bingoMatrices = make([]BingoMatrix, 0)
+var wonBoard BingoMatrix
 
 type BingoMatrix struct {
 	numbers [][]int
-	found   [][]int
+	found   [5][5]int
 }
 
 func FindBingoMatrix() {
@@ -23,10 +26,85 @@ func FindBingoMatrix() {
 	loadData()
 	fmt.Println("Matrices loaded: ", bingoMatrices)
 
+	findWinningMatrix()
+	fmt.Println("The first board that won BINGO (horizontal or vertical) = ", wonBoard, " - Last number called = ", lastNumberCalled)
+
+	sumOfUncalledNumbers := calculateScore()
+	fmt.Println("The sum of all uncalled numbers = ", sumOfUncalledNumbers)
+
+	puzzleAnswer := sumOfUncalledNumbers * lastNumberCalled
+	fmt.Println("The Puzzle answer (Final score) = ", puzzleAnswer)
+
 	fmt.Println("FindBingoMatrix() End")
 }
 
+func calculateScore() int {
+
+	var sumOfUncalledNumbers int
+	for i := 0; i < len(wonBoard.numbers); i++ {
+		for j := 0; j < len(wonBoard.numbers[i]); j++ {
+
+			if wonBoard.found[i][j] == 0 {
+				sumOfUncalledNumbers += wonBoard.numbers[i][j]
+			}
+		}
+	}
+
+	return sumOfUncalledNumbers
+}
+
+func verifyAnyWonBoard() bool {
+
+	for m := 0; m < len(bingoMatrices); m++ {
+
+		for i := 0; i < len(bingoMatrices[m].found); i++ {
+
+			horizontalSum := bingoMatrices[m].found[i][0] + bingoMatrices[m].found[i][1] + bingoMatrices[m].found[i][2] + bingoMatrices[m].found[i][3] + bingoMatrices[m].found[i][4]
+			if horizontalSum == 5 {
+				fmt.Println("Found a winning board!")
+				wonBoard = bingoMatrices[m]
+				return true
+			}
+
+			verticalSum := bingoMatrices[m].found[0][i] + bingoMatrices[m].found[1][i] + bingoMatrices[m].found[2][i] + bingoMatrices[m].found[3][i] + bingoMatrices[m].found[4][i]
+			if verticalSum == 5 {
+				fmt.Println("Found a winning board!")
+				wonBoard = bingoMatrices[m]
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func findWinningMatrix() {
+
+	for _, v := range numbers {
+
+		fmt.Println("Current Number ", v)
+
+		for m := 0; m < len(bingoMatrices); m++ {
+
+			for i := 0; i < len(bingoMatrices[m].numbers); i++ {
+				for j := 0; j < len(bingoMatrices[m].numbers[i]); j++ {
+					if bingoMatrices[m].numbers[i][j] == v {
+						bingoMatrices[m].found[i][j] = 1
+
+						if verifyAnyWonBoard() {
+							lastNumberCalled = v
+							return
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
 func loadData() {
+
+	fmt.Println("loadData() Start")
 
 	file, err := os.Open("day-4/puzzle-input.txt")
 	if err != nil {
@@ -43,7 +121,7 @@ func loadData() {
 
 	// first line are the bingo numbers
 	scanner.Scan()
-	numbers = strings.Split(scanner.Text(), ",")
+	numbers = stringArrayToInt(scanner.Text(), ",")
 	fmt.Println(numbers)
 
 	// next lines will be loaded into matrices
@@ -62,28 +140,23 @@ func loadData() {
 
 		var currentMatrix BingoMatrix
 
-		currentLine := scanner.Text()
-		firstRowMatrix := stringArrayToInt(currentLine)
+		firstRowMatrix := stringArrayToIntWithWhitespace(scanner.Text())
 		currentMatrix.numbers = append(currentMatrix.numbers, firstRowMatrix)
 
 		scanner.Scan()
-		currentLine = scanner.Text()
-		secondRowMatrix := stringArrayToInt(currentLine)
+		secondRowMatrix := stringArrayToIntWithWhitespace(scanner.Text())
 		currentMatrix.numbers = append(currentMatrix.numbers, secondRowMatrix)
 
 		scanner.Scan()
-		currentLine = scanner.Text()
-		thirdRowMatrix := stringArrayToInt(currentLine)
+		thirdRowMatrix := stringArrayToIntWithWhitespace(scanner.Text())
 		currentMatrix.numbers = append(currentMatrix.numbers, thirdRowMatrix)
 
 		scanner.Scan()
-		currentLine = scanner.Text()
-		fourthRowMatrix := stringArrayToInt(currentLine)
+		fourthRowMatrix := stringArrayToIntWithWhitespace(scanner.Text())
 		currentMatrix.numbers = append(currentMatrix.numbers, fourthRowMatrix)
 
 		scanner.Scan()
-		currentLine = scanner.Text()
-		fifthRowMatrix := stringArrayToInt(currentLine)
+		fifthRowMatrix := stringArrayToIntWithWhitespace(scanner.Text())
 		currentMatrix.numbers = append(currentMatrix.numbers, fifthRowMatrix)
 
 		bingoMatrices = append(bingoMatrices, currentMatrix)
@@ -92,11 +165,24 @@ func loadData() {
 	if err := scanner.Err(); err != nil {
 		fmt.Println("ERROR reading file")
 	}
+
+	fmt.Println("loadData() End")
 }
 
-func stringArrayToInt(stringNumbers string) []int {
+func stringArrayToInt(stringNumbers string, separator string) []int {
 
-	stringNumbersArr := strings.Split(stringNumbers, " ")
+	stringNumbersArr := strings.Split(stringNumbers, separator)
+	intArray := make([]int, len(stringNumbersArr))
+
+	for i, v := range stringNumbersArr {
+		intArray[i], _ = strconv.Atoi(v)
+	}
+
+	return intArray
+}
+
+func stringArrayToIntWithWhitespace(stringNumbers string) []int {
+	stringNumbersArr := regexp.MustCompile("[\\s]+").Split(strings.TrimSpace(stringNumbers), -1)
 	intArray := make([]int, len(stringNumbersArr))
 
 	for i, v := range stringNumbersArr {
