4dbeb18a66cb9438111f35856401885b9de643d8
Kevork <ksayad@gmail.com>
2022-12-14 00:30:43 -0500

    day 5 wip

--- day-5/navigate.go
+++ day-5/navigate.go
@@ -9,49 +9,46 @@ import (
 )
 
 var lines = make([]Line, 0)
-var linesIntersects = make([]LineIntersect, 0)
+var m = make(map[Coordinate]int)
 
 type Line struct {
-	x1 int
-	y1 int
-	x2 int
-	y2 int
+	p1 Coordinate
+	p2 Coordinate
+}
+
+type Coordinate struct {
+	x int
+	y int
 }
 
 /*
 Horizontal line = same Y value
 */
 func (line Line) isHorizontal() bool {
-	return line.y1 == line.y2
+	return line.p1.y == line.p2.y
 }
 
 /*
 Vertical line = same X value
 */
 func (line Line) isVertical() bool {
-	return line.x1 == line.x2
-}
-
-type LineIntersect struct {
-	l1 Line
-	l2 Line
+	return line.p1.x == line.p2.x
 }
 
 func FindOverlaps() {
 
-	fmt.Println("FindLeastOverlap() Start")
+	fmt.Println("FindOverlaps() Start")
 
-	loadData()
-	findIntersects()
+	readFile()
+	loadMap()
+	countIntersectsGreaterThan(2)
 
-	fmt.Println("Number of intersects of two lines: ", len(linesIntersects))
-
-	fmt.Println("FindLeastOverlap() End")
+	fmt.Println("FindOverlaps() End")
 }
 
-func loadData() {
+func readFile() {
 
-	fmt.Println("loadData() Start")
+	fmt.Println("readFile() Start")
 
 	file, err := os.Open("day-5/day5-input.txt")
 	if err != nil {
@@ -69,167 +66,67 @@ func loadData() {
 
 	for scanner.Scan() {
 
+		// Line = (x1  res[0][1], y1 res[0][2]) -> (x2 res[0][3], y2 res[0][4])
 		line := scanner.Text()
-		strToLine(line, r)
+
+		if r.MatchString(line) {
+
+			res := r.FindAllStringSubmatch(line, 1)
+
+			x1, _ := strconv.Atoi(res[0][1])
+			y1, _ := strconv.Atoi(res[0][2])
+			x2, _ := strconv.Atoi(res[0][3])
+			y2, _ := strconv.Atoi(res[0][4])
+
+			if x1 == x2 || y1 == y2 {
+				lines = append(lines, Line{p1: Coordinate{x1, y1}, p2: Coordinate{x2, y2}})
+			}
+
+		} else {
+			fmt.Println("ERROR - Line (", line, ") does not match regex")
+		}
 	}
 
 	fmt.Println("Parsed ", len(lines), " of strings as Line structs")
-	fmt.Println("loadData() End")
+	fmt.Println("readFile() End")
 }
 
-func strToLine(line string, r *regexp.Regexp) {
+func loadMap() {
 
-	if r.MatchString(line) {
+	for _, v := range lines {
 
-		res := r.FindAllStringSubmatch(line, 1)
+		if v.isHorizontal() {
 
-		// Line = (x1  res[0][1], y1 res[0][2]) -> (x2 res[0][3], y2 res[0][4])
+			lowerX := min(v.p1.x, v.p2.x)
+			upperX := max(v.p1.x, v.p2.x)
 
-		var line Line
-		line.x1, _ = strconv.Atoi(res[0][1])
-		line.y1, _ = strconv.Atoi(res[0][2])
-		line.x2, _ = strconv.Atoi(res[0][3])
-		line.y2, _ = strconv.Atoi(res[0][4])
+			for i := lowerX; i <= upperX; i++ {
+				m[Coordinate{x: i, y: v.p1.y}]++
+			}
 
-		lines = append(lines, line)
-	} else {
-		fmt.Println("ERROR - Line does not match regex: ", line)
-	}
-}
+		} else if v.isVertical() {
 
-func findIntersects() {
+			lowerY := min(v.p1.y, v.p2.y)
+			upperY := max(v.p1.y, v.p2.y)
 
-	for _, l1 := range lines {
-		for _, l2 := range lines {
-
-			// skip if current lines refer to each-other
-			if l1 != l2 {
-				calc(l1, l2)
+			for i := lowerY; i <= upperY; i++ {
+				m[Coordinate{x: v.p1.x, y: i}]++
 			}
 		}
 	}
 }
 
-func calc(l1, l2 Line) {
+func countIntersectsGreaterThan(minIntersects int) {
 
-	// Line 1 = (l1x1,l1y1) -> (l1x2,l1y2) and Line 2 = (l2x1,l2y1) -> (l2x2,l2y2)
+	sumOfIntersects := 0
 
-	if l1.isHorizontal() {
-
-		if l2.isHorizontal() {
-			parallelHorizontalLinesOverlap(l1, l2)
-		} else if l2.isVertical() {
-			perpendicularLinesOverlap(l1, l2)
-		}
-	} else if l1.isVertical() {
-		if l2.isHorizontal() {
-			perpendicularLinesOverlap(l2, l1)
-		} else if l2.isVertical() {
-			parallelVerticalLinesOverlap(l1, l2)
-		}
-	}
-}
-
-func parallelHorizontalLinesOverlap(l1, l2 Line) {
-
-	if l1.y1 == l2.y1 {
-
-		lower := min(l2.x1, l2.x2)
-		upper := max(l2.x1, l2.x2)
-
-		line1X1OverlapLine2 := coordinateWithinBoundary(l1.x1, lower, upper)
-		line1X2OverlapLine2 := coordinateWithinBoundary(l1.x2, lower, upper)
-
-		lower = min(l1.x1, l1.x2)
-		upper = max(l1.x1, l1.x2)
-
-		line2X1OverlapLine1 := coordinateWithinBoundary(l2.x1, lower, upper)
-		line2X2OverlapLine1 := coordinateWithinBoundary(l2.x2, lower, upper)
-
-		overlap := line1X1OverlapLine2 ||
-			line1X2OverlapLine2 ||
-			line2X1OverlapLine1 ||
-			line2X2OverlapLine1
-
-		if overlap {
-			addIntersectToList(l1, l2)
-		}
-	}
-}
-
-func parallelVerticalLinesOverlap(l1, l2 Line) {
-
-	if l1.x1 == l2.x1 {
-
-		lower := min(l2.y1, l2.y2)
-		upper := max(l2.y1, l2.y2)
-
-		line1Y1OverlapLine2 := coordinateWithinBoundary(l1.y1, lower, upper)
-		line1Y2OverlapLine2 := coordinateWithinBoundary(l1.y2, lower, upper)
-
-		lower = min(l1.y1, l1.y2)
-		upper = max(l1.y1, l1.y2)
-
-		line2Y1OverlapLine1 := coordinateWithinBoundary(l2.y1, lower, upper)
-		line2Y2OverlapLine1 := coordinateWithinBoundary(l2.y2, lower, upper)
-
-		overlap := line1Y1OverlapLine2 ||
-			line1Y2OverlapLine2 ||
-			line2Y1OverlapLine1 ||
-			line2Y2OverlapLine1
-
-		if overlap {
-			addIntersectToList(l1, l2)
-		}
-	}
-}
-
-func perpendicularLinesOverlap(horizontalLine, verticalLine Line) {
-
-	verticalLowerBoundary := min(verticalLine.y1, verticalLine.y2)
-	verticalUpperBoundary := max(verticalLine.y1, verticalLine.y2)
-
-	xStartLoc := min(horizontalLine.x1, horizontalLine.x2)
-	xEndLoc := max(horizontalLine.x2, horizontalLine.x2)
-
-	for i := xStartLoc; i <= xEndLoc; i++ {
-
-		if i == verticalLine.x1 && horizontalLine.y1 >= verticalLowerBoundary && horizontalLine.y1 <= verticalUpperBoundary {
-			addIntersectToList(horizontalLine, verticalLine)
-			break
-		}
-	}
-}
-
-func coordinateWithinBoundary(ref, lower, upper int) bool {
-	return ref >= lower && ref <= upper
-}
-
-/*
-Append the intersect of the two lines only if it ha snot already been saved.
-*/
-func addIntersectToList(l1, l2 Line) {
-
-	//fmt.Println("Both lines (l1 ", l1, ", l2 ", l2, ") intersect")
-
-	if !isIntersectInList(l1, l2) {
-		linesIntersects = append(linesIntersects, LineIntersect{l1: l1, l2: l2})
-	}
-}
-
-func isIntersectInList(l1, l2 Line) bool {
-
-	var found = false
-
-	for i := range linesIntersects {
-		if (linesIntersects[i].l1 == l1 && linesIntersects[i].l2 == l2) ||
-			linesIntersects[i].l1 == l2 && linesIntersects[i].l2 == l1 {
-			found = true
-			break
+	for _, v := range m {
+		if v >= minIntersects {
+			sumOfIntersects++
 		}
 	}
 
-	return found
+	fmt.Println("The number of coordinates with intersects greater than ", minIntersects, " is ", sumOfIntersects)
 }
 
 func min(a, b int) int {
